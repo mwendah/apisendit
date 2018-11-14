901a572063ac69673fe613a06ad5d3f84548096f
Mwenda <mwendak3@gmail.com>
2018-11-14 09:28:49 +0300

    Add files via upload

---
+++ app/tests/v1/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from ..api.v1 import version1
+
+
+def  create_app():
+	app=Flask (__name__ instance_relative_config=True)
+	app.config.from_pyfile('config.py')
+	app._register_blueprint(version1)
+return app
